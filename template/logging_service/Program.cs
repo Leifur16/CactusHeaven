a6d3f0cf0b800693604c335d2354e60c91a55058
Leifur16 <leifur16@ru.is>
2018-11-01 12:46:37 +0000

    tibi

--- template/logging_service/Program.cs
+++ template/logging_service/Program.cs
@@ -10,38 +10,46 @@ namespace loggingService
     {
         public static void Main()
         {
+            var exchangeName = "order_exchange";
+            //var queueName = "logging_queue";
+            var inputRoutingKey = "create_order";
+
             var factory = new ConnectionFactory() { HostName = "localhost" };
-            using(var connection = factory.CreateConnection())
-            using(var channel = connection.CreateModel())
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "hello",
-                                    durable: false,
-                                    exclusive: false,
-                                    autoDelete: false,
-                                    arguments: null);
+                // Declare exchange
+                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+
+                // Declare Queu
+                var queueName = channel.QueueDeclare().QueueName;
+
+                // Bind queue to exchange with a routing key
+                channel.QueueBind(queue: queueName,
+                                  exchange: exchangeName,
+                                  routingKey: inputRoutingKey);
 
                 var consumer = new EventingBasicConsumer(channel);
+
                 consumer.Received += (model, ea) =>
                 {
                     var body = ea.Body;
                     var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received {0}", message);
+                    Console.WriteLine(message);
+
+                    string path = Directory.GetCurrentDirectory();
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, "log.txt"), true))
+                    {
+                        //outputFile.Write("Log: ");
+                        //outputFile.Write(message);
+                        outputFile.WriteLine("Log: " + message);
+                    }
                 };
-                channel.BasicConsume(queue: "logging_queue",
+                channel.BasicConsume(queue: queueName,
                                     autoAck: true,
                                     consumer: consumer);
 
-                // Create a string array with the lines of text
-               string[] lines = { "First line", "Second line", "Third line" };
-
-                // Set a variable to the My Documents path.
-                //string mydocpath = Environment.GetFolderPath(); thetta a orugglega ad vera 
-
-                // Write the string array to a new file named "WriteLines.txt".
-              /*   using (StreamWriter outputFile = new StreamWriter(Path.Combine(mydocpath,"WriteLines.txt"))) {
-                    foreach (string line in lines)
-                        outputFile.WriteLine(line);
-                }*/
+                Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
             }
         }
